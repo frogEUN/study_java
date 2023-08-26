1d99aae9140ac6eff9852542564a38218a5e0f61
frogEUN <eunhasu801@gmail.com>
2023-08-26 23:12:38 +0900

    java first

---
+++ src/First/First.java
@@ -0,0 +1,12 @@
+package First;
+
+public class First {
+    // 클래스 > 메서드 > 실행문
+    // 클래스 : 객체 지향 언어세어 프로그램을 개발하는 단위. 적어도 하나의 클래스 필요.
+    // method 메서드 : 코드의 모임
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+
+    }
+}
