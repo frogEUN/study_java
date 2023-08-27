37f034de5d5c13a5bc31b140bf91646c150a39e3
frogEUN <eunhasu801@gmail.com>
2023-08-27 22:31:56 +0900

    use method

--- src/First/First.java
+++ src/First/First.java
@@ -6,7 +6,22 @@ public class First {
     // method 메서드 : 코드의 모임
     public static void main(String[] args){
         System.out.println("Hello World");
+        int weight;  // 변수 선언
+        weight = 50;  // 변수 초기화
+        // 초깃값을 통하여 데이터 타입을 추론할 수 있는 var
+        var number = 100;  // 메서드 내부에 있는 변수에 대해서만 사용
+        final double PI;  // 상수, 프로그램 실행 도중 변경할 수 없는 데이터
 
+        System.out.println(Holjjak(1));
 
+    }
+
+    public static String Holjjak(int n){
+        if (n%2 ==0){
+            return "짝수";
+        }
+        else{
+            return "홀수";
+        }
     }
 }
