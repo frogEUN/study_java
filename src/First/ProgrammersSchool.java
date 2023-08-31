40dbd23f7cd2b86b6db49ecd5ef4abfd649c08fb
frogEUN <eunhasu801@gmail.com>
2023-08-31 20:33:33 +0900

    코딩테스트 입문 Day3 (2) - 해답
    최빈값 구하기

--- src/First/ProgrammersSchool.java
+++ src/First/ProgrammersSchool.java
@@ -1,57 +1,35 @@
 package First;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 public class ProgrammersSchool {
-
-    public static void main(String args[]){
-        System.out.println(Solution3.solution(new int[]{1, 1, 1, 1}));
-    }
 }
 
 // 최빈값 구하기
-// *내가 풀어본 답
-class Solution3 {
-    public static int solution(int[] array) {
-        if(array.length == 1)
-            return array[0];
-        ArrayList<ArrayList<Integer>> nums = new ArrayList<>();
-        ArrayList<Integer> check = new ArrayList<Integer>();
-        for (int i = 0; i < array.length; i++){
-            if (Collections.frequency(check, array[i]) == 0){
-                check.add(array[i]);
-                var temp = new ArrayList<Integer>();
-                temp.add(array[i]);
-                var count = 0;
-                for (int a : array){
-                    if(a == array[i])
-                        count++;
-                }
-                temp.add(count);
-                if(nums.size() == 0){
-                    nums.add(temp);
-                }
-                else {
-                    for(int l = 0; l < nums.size(); l++){
-                        // 정렬해서 넣기
-                        if (temp.get(1) >= nums.get(l).get(1)){
-                            nums.add(l, temp);
-                            break;
-                        }
-                        if (l==nums.size()-1){
-                            nums.add(temp);
-                        }
-                    }
-                }
+// *해답
 
+class Solution {
+    public int solution(int[] array) {
+        int maxCount = 0;  // 가장 많이 등장한 횟수
+        int answer = 0;   // 가장 많이 등장한 수
+
+        // 각 숫자의 등장 횟수를 저장할 해시맵
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // array를 순회하면서
+        for(int number : array){
+            int count = map.getOrDefault(number, 0) + 1;
+            // getOrDefault -> 주어진 key에 해당하는 값을 가져오되 없다면 기본값을 반환
+
+            if(count > maxCount){
+                maxCount = count;
+                answer = number;
             }
+            else  if(count == maxCount){
+                // 다중 최빈값 처리
+                answer = -1;
+            }
+            map.put(number, count);
         }
-        if(nums.size() >= 2 && nums.get(0).get(1).equals(nums.get(1).get(1))){
-            return -1;
-        }
-        else{
-            return nums.get(0).get(0);
-        }
-
+        return answer;
     }
 }
