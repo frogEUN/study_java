cc94f86a129607f81f10957b7be5f4310688e480
frogEUN <eunhasu801@gmail.com>
2023-09-02 23:11:03 +0900

    2438번

--- src/First/Baekjoon.java
+++ src/First/Baekjoon.java
@@ -1,27 +1,22 @@
 package First;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
-// 11021번
+// 2438번
 // 문제 요약
-// 입력 : 테스트 케이스 개수, 각 줄 마다 A B
-// 출력 : Case #x: A + B = C
+// 입력 : N
+// 출력 : 첫째 줄부터 N번째줄까지 1~n개 * 출력
 public class Baekjoon {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int t = Integer.parseInt(br.readLine());
-        for (int i = 0; i < t; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            String result = "Case #" + (i+1) + ": " + a + " + " + b + " = " + (a+b);
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++){
+            String result = "*".repeat(i);
             bw.write(result);
             bw.newLine();
         }
         bw.flush();
         bw.close();
-
     }
 }
