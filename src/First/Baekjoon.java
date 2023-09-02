53539a9baa7e5dc98ecffc03fc34704f7393d372
frogEUN <eunhasu801@gmail.com>
2023-09-02 23:01:50 +0900

    11021번

--- src/First/Baekjoon.java
+++ src/First/Baekjoon.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 // 11021번
 // 문제 요약
 // 입력 : 테스트 케이스 개수, 각 줄 마다 A B
-// 출력 : Case #x: A+B
+// 출력 : Case #x: A + B = C
 public class Baekjoon {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,12 +16,12 @@ public class Baekjoon {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            String result = "Case #" + (i+1) + ": " + (a + b);
+            String result = "Case #" + (i+1) + ": " + a + " + " + b + " = " + (a+b);
             bw.write(result);
             bw.newLine();
         }
         bw.flush();
         bw.close();
-    }
 
+    }
 }
