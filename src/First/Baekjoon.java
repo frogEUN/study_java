88e078950ae50bbd1088400650b332229bf17da3
frogEUN <eunhasu801@gmail.com>
2023-09-05 19:08:43 +0900

    10811번

--- src/First/Baekjoon.java
+++ src/First/Baekjoon.java
@@ -3,21 +3,39 @@ package First;
 import java.io.*;
 import java.util.*;
 
-// 3052번
+// 10811번
 public class Baekjoon {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        List<Integer> nums = new ArrayList<>();
-        for(int i=0; i<10; i++){
-            int n = Integer.parseInt(br.readLine()) % 42;
-            if(!nums.contains(n)){
-                nums.add(n);
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int size = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int[] baskets = new int[size];
+        for(int i=0; i < size; i++){
+            baskets[i] = i+1;
         }
-        bw.write(String.valueOf(nums.size()));
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            int one = Integer.parseInt(st.nextToken())-1;
+            int two = Integer.parseInt(st.nextToken())-1;
+            int[] temp = new int[two-one+1];
+            for(int q = 0, w = two; q < temp.length && w >= one; q++, w--){
+                temp[q] = baskets[w];
+            }
+            for(int j=0, k=one; j<two-one+1 && k <= two; j++, k++){
+                baskets[k] = temp[j];
+            }
+
+        }  // end of for(n)
+        String result = "";
+        for(int i=0; i<baskets.length; i++){
+            result = result + baskets[i] + " ";
+        }
+        bw.write(result);
         bw.flush();
         bw.close();
         br.close();
-    }
+    }  // end of main
+
 }
