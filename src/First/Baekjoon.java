8b0db45e894e27786f2aa727db9403351eb97d1c
frogEUN <eunhasu801@gmail.com>
2023-09-06 12:17:09 +0900

    1546번

--- src/First/Baekjoon.java
+++ src/First/Baekjoon.java
@@ -3,39 +3,28 @@ package First;
 import java.io.*;
 import java.util.*;
 
-// 10811번
+// 1546번
 public class Baekjoon {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        double[] scores = new double[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int size = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-        int[] baskets = new int[size];
-        for(int i=0; i < size; i++){
-            baskets[i] = i+1;
-        }
+        double max = 0;
         for(int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
-            int one = Integer.parseInt(st.nextToken())-1;
-            int two = Integer.parseInt(st.nextToken())-1;
-            int[] temp = new int[two-one+1];
-            for(int q = 0, w = two; q < temp.length && w >= one; q++, w--){
-                temp[q] = baskets[w];
+            double temp = (double)Integer.parseInt(st.nextToken());
+            scores[i] = temp;
+            if(temp>=max){
+                max = temp;
             }
-            for(int j=0, k=one; j<two-one+1 && k <= two; j++, k++){
-                baskets[k] = temp[j];
-            }
-
-        }  // end of for(n)
-        String result = "";
-        for(int i=0; i<baskets.length; i++){
-            result = result + baskets[i] + " ";
         }
-        bw.write(result);
-        bw.flush();
-        bw.close();
-        br.close();
+        double sum = 0;
+        for(int i = 0; i < scores.length; i++){
+            scores[i] = (scores[i] / max) * 100.0;
+            sum += scores[i];
+        }
+        System.out.print(String.valueOf(sum / n));
     }  // end of main
+
 
 }
