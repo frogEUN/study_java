5c84e611d4d82cbd6c7e47e5d3faad9406077859
frogEUN <eunhasu801@gmail.com>
2023-09-09 21:33:59 +0900

    2738번

--- src/first/Baekjoon.java
+++ src/first/Baekjoon.java
@@ -1,24 +1,49 @@
 package first;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
-// 10988번
+// 2738번
 public class Baekjoon {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(temp(br.readLine().toCharArray()));
-        br.close();
-    }
-
-    private static int temp(char[] word){
-        int l = word.length;
-        for (int i = 0; i<l; i++){
-            if(i == l-1-i) break;
-            if(word[i] != word[l-1-i]){
-                return 0;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] one = new int[n][m];
+        int[][] two = new int[n][m];
+        for(int nn = 0; nn < n; nn++){
+            st = new StringTokenizer(br.readLine());
+            for(int mm = 0; mm < m; mm++){
+                one[nn][mm] = Integer.parseInt(st.nextToken());
             }
         }
-        return 1;
+        for(int nn = 0; nn < n; nn++){
+            st = new StringTokenizer(br.readLine());
+            for(int mm = 0; mm < m; mm++){
+                two[nn][mm] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int[][] result = new int[n][m];
+        for(int nn = 0; nn < n; nn++){
+            for(int mm = 0; mm < m; mm++){
+                result[nn][mm] = one[nn][mm] + two[nn][mm];
+            }
+        }
+
+        for(int nn = 0; nn < n; nn++){
+            for(int mm = 0; mm < m; mm++){
+                bw.write(String.valueOf(result[nn][mm]) + " ");
+            }
+            bw.newLine();
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+
     }
+
 
 }
