ede48d8a3e528743e9d8215ccf3c492afa5e2a26
frogEUN <eunhasu801@gmail.com>
2023-09-14 23:01:36 +0900

    IDK

--- src/first/Baekjoon.java
+++ src/first/Baekjoon.java
@@ -1,27 +1,10 @@
 package first;
 
-import java.io.*;
-import java.util.StringTokenizer;
+
 
 // 5086번
 public class Baekjoon {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        while(true){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            if(a == 0 && b == 0) break;
-            if(b % a == 0) bw.write("factor");
-            else if(a % b == 0) bw.write("multiple");
-            else bw.write("neither");
-            bw.newLine();
-        }
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+
 
 
 
